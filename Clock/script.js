7d9e8f4da1c84fdbe80454ec65ea798c53e50955
Satmeet_Singh <satmeetarora@gmail.com>
2024-01-25 12:43:40 +0530

    Second js project-Clock

---
+++ Clock/script.js
@@ -0,0 +1,20 @@
+let second = document.getElementById("second-hand");
+let minute = document.getElementById("minute-hand");
+let hour = document.getElementById("hour-hand");
+
+function setDate() {
+  let today = new Date();
+  let Second = today.getSeconds();
+  let Minute = today.getMinutes();
+  //   let Hour = today.getHours();
+  let Hour = 15;
+
+  const secondDegres = 90 + (Second / 60) * 360;
+  const minuteDegres = 90 + (Minute / 60) * 360;
+  const hourDegrees = 90 + (Minute / 720) * 360 + (Hour / 12) * 360;
+  hour.style.transform = `rotate(${hourDegrees}deg)`;
+  second.style.transform = `rotate(${secondDegres}deg)`;
+  minute.style.transform = `rotate(${minuteDegres}deg)`;
+}
+
+setInterval(setDate, 1000);
