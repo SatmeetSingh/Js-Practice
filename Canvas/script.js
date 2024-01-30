d47fe7273437cb4a7c2f3c7c1962e03e43801fe0
Satmeet_Singh <satmeetarora@gmail.com>
2024-01-30 11:18:00 +0530

    little change in canvas

--- Canvas/script.js
+++ Canvas/script.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById("draw");
 const colorChange = document.querySelector(".color .input1");
 const lineWidth = document.querySelector(".color .input2");
 const button = document.querySelector(".color1 .btn");
+const Hue = document.querySelector(".color1 .btn1");
+
 let ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 ctx.strokeStyle = "#BadAss";
@@ -14,6 +16,7 @@ let lastY = window.offsetX;
 let lastX = window.offsetY;
 
 colorChange.addEventListener("change", (e) => {
+  canvas.removeEventListener("mousemove", HueDraw);
   let changedColor = e.target.value;
   ctx.strokeStyle = changedColor;
 });
@@ -23,7 +26,19 @@ lineWidth.addEventListener("change", (e) => {
   ctx.lineWidth = changeWidth;
 });
 
-button.addEventListener("click", () => (ctx.strokeStyle = "#fff"));
+let hue = 0;
+
+function HueDraw(e) {
+  if (isDrawing) {
+    ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`;
+    ctx.beginPath();
+    ctx.moveTo(lastX, lastY);
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.stroke();
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+    hue++;
+  }
+}
 
 function draw(e) {
   if (isDrawing) {
@@ -34,11 +49,33 @@ function draw(e) {
     [lastX, lastY] = [e.offsetX, e.offsetY];
   }
 }
+
 canvas.addEventListener("mousedown", (e) => {
   isDrawing = true;
   [lastX, lastY] = [e.offsetX, e.offsetY];
 });
 
-canvas.addEventListener("mousemove", draw);
 canvas.addEventListener("mouseup", () => (isDrawing = false));
 canvas.addEventListener("mouseout", () => (isDrawing = false));
+
+button.addEventListener("click", () => {
+  canvas.removeEventListener("mousemove", HueDraw);
+  ctx.lineJoin = "square";
+  ctx.lineCap = "square";
+  ctx.strokeStyle = "#fff";
+});
+
+let useHueDraw = false;
+
+Hue.addEventListener("click", () => {
+  useHueDraw = !useHueDraw;
+  if (useHueDraw) {
+    canvas.addEventListener("mousemove", HueDraw);
+  } else {
+    canvas.removeEventListener("mousemove", HueDraw);
+  }
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  draw(e);
+});
